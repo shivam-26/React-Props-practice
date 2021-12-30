19923dc53a0ac94ce39a0c2855c23eb1ba7c38b9
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-31 00:15:51 +0530

    React Prop Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img" src={props.src} alt="avatar_img" />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.number}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name="Emilly"
+        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        number="8374897283"
+        email="sjfbsjk@jsjlk"
+      />
+
+      <Card
+        name="Bond"
+        src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        number="5873897839"
+        email="hshjs@bsjbsj"
+      />
+      <Card
+        name="Chuck Norris"
+        src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        number="8573897883"
+        email="hhdwuih@jcsbjs"
+      />
     </div>
   );
 }
